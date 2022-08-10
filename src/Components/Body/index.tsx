7b0460bdfefd50be6eea3942399f0f92c362af7c
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-08-10 08:00:19 -0300

    feat: README.md

--- src/Components/Body/index.tsx
+++ src/Components/Body/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {BodyContainer } from './styles';
 import { FaHeart } from 'react-icons/fa';
 import { AiFillCloseCircle } from 'react-icons/ai';
